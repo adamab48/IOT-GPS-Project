9057025f49782b1c4eadf3846bd87c0ce7198968
adamab48 <abichouadam48@gmail.com>
2022-07-21 13:11:28 +0100

    First Working Version

---
+++ GPS_1.0.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import RPi.GPIO as GPIO
+
+import serial
+import time
+
+ser = serial.Serial('/dev/ttyS0',115200)
+ser.flushInput()
+
+power_key = 4
+rec_buff2 = ''
+time_count = 0
+
+
+def send_at_input() :
+    buffer = ''
+    command_input = input('Please input the AT command:')
+    ser.write((command_input+  '\r\n' ).encode())
+    time.sleep(0.1)
+    if ser.inWaiting():
+        time.sleep(0.01)
+        buffer = ser.read(ser.inWaiting())
+    if buffer != '':
+        print(buffer.decode())
+        buffer = ''
+def send_at(command) :
+    buffer2 = ''
+    response = ''
+    ser.write((command+  '\r\n' ).encode())
+    time.sleep(0.1)
+    if ser.inWaiting():
+        time.sleep(0.01)
+        buffer2 = ser.read(ser.inWaiting())
+        response = buffer2.decode()
+    if buffer2 != '' and ('+CGNSINF: ' in response):
+        GPSDATA = str(response)
+        new  = GPSDATA[GPSDATA.index("CGNSINF: ")+len("CGNSINF: "):]
+        print("Coordinates : [",new[23:42],"]")
+        buffer2 = ''
+    else :
+        print("[WARNING]---[No Coordinates Recieved !]")
+        buffer2 = ''
+
+def power_on(power_key):
+    print('GPS Module is starting:')
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(power_key,GPIO.OUT)
+	time.sleep(0.1)
+	GPIO.output(power_key,GPIO.HIGH)
+	time.sleep(2)
+	GPIO.output(power_key,GPIO.LOW)
+	time.sleep(2)
+	ser.flushInput()
+	print('GPS Module is ready')
+
+
+def power_down(power_key):
+	print('GPS Module is logging off:')
+	GPIO.output(power_key,GPIO.HIGH)
+	time.sleep(3)
+	GPIO.output(power_key,GPIO.LOW)
+	time.sleep(2)
+	print('Good bye')
+def main() :
+    send_at("AT+CGNSPWR=1")
+    time.sleep(2)
+    for i in range(10) :
+        print("[INFO]----[READING NUMBER ",i,"]")
+        send_at("AT+CGNSINF")
+        time.sleep(1)
+power_on(power_key)
+while True :
+    try :
+        ser = serial.Serial('/dev/ttyS0',115200)
+        ser.flushInput()
+        main()
+    except OSError as e:
+        print("[WARNING][Restablishing Connection ...]")
+        ser.close()
+    except KeyboardInterrupt :
+        print("[INTERRUPT]--[Keyboeard Interrupt]")
+        power_down(power_key)
+        break
+    except :
+        print("[ERROR]----[Another Error]")
+        power_down(power_key)
